19cce303fadd8482095417e64eb73764114eca02
anjireddy2 <anjireddy0537@gmail.com>
2019-09-26 17:29:17 +0530

    Uncommented the relationsips

--- app/models/water_body_detail.rb
+++ app/models/water_body_detail.rb
@@ -1,7 +1,7 @@
 class WaterBodyDetail < ApplicationRecord
     
-    # belongs_to :seasonality
-    # belongs_to :water_body_type
-    # belongs_to :water_body_ownership
-    # belongs_to :water_body
+    belongs_to :seasonality
+    belongs_to :water_body_type
+    belongs_to :water_body_ownership
+    belongs_to :water_body
 end
