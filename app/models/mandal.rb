7091575f10d647956951b857c173e91dc65012d2
anjireddy2 <anjireddy0537@gmail.com>
2019-09-27 13:11:47 +0530

    Commented relationships

--- app/models/mandal.rb
+++ app/models/mandal.rb
@@ -1,12 +1,12 @@
 class Mandal < ApplicationRecord
     
     belongs_to :district
-    has_many :panchayats
-    has_many :villages
-    has_many :water_bodies
-    has_many :vessel_detials
-    has_many :fish_landing_centers
+    # has_many :panchayats
+    # has_many :villages
+    # has_many :water_bodies
+    # has_many :vessel_detials
+    # has_many :fish_landing_centers
 
-    scope :flc_mandals, -> { joins(:fish_landing_centers).where('fish_landing_centers.mandal_id > 0') }
+    # scope :flc_mandals, -> { joins(:fish_landing_centers).where('fish_landing_centers.mandal_id > 0') }
 
 end
--- app/models/society_member.rb
+++ app/models/society_member.rb
@@ -1,5 +1,5 @@
 class SocietyMember < ApplicationRecord
-    belongs_to :society_registration
-    validates :member_aadhaar_no,:uniqueness => true
-    validates_presence_of :member_ration_card_no
+    # belongs_to :society_registration
+    # validates :member_aadhaar_no,:uniqueness => true
+    # validates_presence_of :member_ration_card_no
 end
