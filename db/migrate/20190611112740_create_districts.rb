8787535642fb0354052107fae8eb40c397d03cc6
anjireddy2 <anjireddy0537@gmail.com>
2019-10-04 18:04:31 +0530

    Changed column type

--- db/migrate/20190611112740_create_districts.rb
+++ db/migrate/20190611112740_create_districts.rb
@@ -2,7 +2,8 @@ class CreateDistricts < ActiveRecord::Migration[5.2]
   def change
     create_table :districts do |t|
         t.integer :district_code
-    	t.string :district_name
+      t.string :district_name
+      t.boolean :is_coastal
      t.timestamps
     end
   end
