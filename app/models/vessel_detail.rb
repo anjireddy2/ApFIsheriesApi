ba87f90fb6c5b94f6aa7636a57e69f9f95491ebb
Anjireddy <anjireddy0537@gmail.com>
2019-10-26 14:43:54 +0530

    Validations

--- app/models/vessel_detail.rb
+++ app/models/vessel_detail.rb
@@ -6,11 +6,11 @@ class VesselDetail < ApplicationRecord
     belongs_to :fish_landing_center
     has_many :vessel_crew_members
 
-#     validates_presence_of :owner_name,:boat_id,:license_renewed_date
-#     validates_presence_of :license_valid_upto,:father_name
-#     validates_presence_of :mobile_number,:father_name,:aadhaar_no
-#    validates_presence_of :bank_account_number,:ifsc_code#,:mfid_number
-#    validates_presence_of :bank_details,:is_eligible
+    validates_presence_of :owner_name,:boat_id,:license_renewed_date
+    validates_presence_of :license_valid_upto,:father_name
+    validates_presence_of :mobile_number,:father_name,:aadhaar_no
+   validates_presence_of :bank_account_number,:ifsc_code#,:mfid_number
+   validates_presence_of :bank_details,:is_eligible
 #    validates :boat_id,:uniqueness => true
 
    scope :un_verified_vessels, -> { where(:member_aadhaar_ref_id => nil) }
